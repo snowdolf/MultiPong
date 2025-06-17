6bed8ab0e25a8890c1496a417990c40426fac61b
snowdolf <snowdolf99@naver.com>
2025-06-17 14:59:43 +0900

    Feat: Show User Email

---
+++ MultiPong/Assets/Scripts/PlayerNameText.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerNameText : MonoBehaviour
+{
+    private Text nameText;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        nameText = GetComponent<Text>();
+
+        if (AuthManager.User != null)
+        {
+            nameText.text = $"Hi, {AuthManager.User.Email}!";
+        }
+        else
+        {
+            nameText.text = "ERROR: User not found!";
+        }
+    }
+}
