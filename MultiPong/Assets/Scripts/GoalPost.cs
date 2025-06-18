d0f0aff43dc74505637700d9795a3ae3b82a8bda
snowdolf <snowdolf99@naver.com>
2025-06-18 19:45:15 +0900

    Feat: Main UI

---
+++ MultiPong/Assets/Scripts/GoalPost.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class GoalPost : MonoBehaviour
+{
+    public int playerNumber;
+}
---
+++ MultiPong/Assets/Scripts/InputButton.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+public class InputButton : MonoBehaviour
+{
+    public static float HorizontalInput;
+
+    public enum State
+    {
+        None,
+        Left,
+        Right
+    }
+
+    private State state = State.None;
+
+    private void Update()
+    {
+        if (state == State.None)
+        {
+            HorizontalInput = 0f;
+        }
+        else if (state == State.Left)
+        {
+            HorizontalInput = -0.5f;
+        }
+        else if (state == State.Right)
+        {
+            HorizontalInput = 0.5f;
+        }
+    }
+
+    public void OnMoveLeftButtonPressed()
+    {
+        state = State.Left;
+    }
+
+    public void OnMoveLeftButtonUp()
+    {
+        if (state == State.Left)
+        {
+            state = State.None;
+        }
+    }
+
+    public void OnMoveRightButtonPressed()
+    {
+        state = State.Right;
+    }
+
+    public void OnMoveRightButtonUp()
+    {
+        if (state == State.Right)
+        {
+            state = State.None;
+        }
+    }
+}
