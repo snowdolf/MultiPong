111e21f47ea766a11c7549d81cbcda6b71f563e8
snowdolf <snowdolf99@naver.com>
2025-06-19 17:22:39 +0900

    Feat: Ball

--- MultiPong/Assets/Scripts/Ball.cs
+++ MultiPong/Assets/Scripts/Ball.cs
@@ -1,16 +1,45 @@
+using Photon.Pun;
 using UnityEngine;
 
-public class Ball : MonoBehaviour
+public class Ball : MonoBehaviourPun
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public bool IsMasterClientLocal => PhotonNetwork.IsMasterClient && photonView.IsMine;
 
-    // Update is called once per frame
-    void Update()
+    private Vector2 direction = Vector2.up;
+    private readonly float speed = 5f;
+    private readonly float randomReflectionIntensity = 0.1f;
+
+    private void FixedUpdate()
     {
-        
+        if (!IsMasterClientLocal || PhotonNetwork.PlayerList.Length < 2)
+        {
+            return;
+        }
+
+        var distance = speed * Time.fixedDeltaTime;
+        var hit = Physics2D.Raycast(transform.position, direction, distance);
+
+        if (hit.collider != null)
+        {
+            var goalPost = hit.collider.GetComponent<GoalPost>();
+
+            if (goalPost != null)
+            {
+                if (goalPost.playerNumber == 1)
+                {
+                    GameManager.Instance.AddScore(2, 1);
+                }
+                else if (goalPost.playerNumber == 2)
+                {
+                    GameManager.Instance.AddScore(1, 1);
+                }
+
+            }
+
+            direction = Vector2.Reflect(direction, hit.normal);
+            direction += Random.insideUnitCircle * randomReflectionIntensity;
+        }
+
+        transform.position += (Vector3)(direction * distance);
     }
 }
